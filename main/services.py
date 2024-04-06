4df4086b415c6fd4d039ea65a0d4c54f3df7d5f3
scroco158 <scroco@mail.ru>
2024-04-06 21:38:18 +0300

    отправка по периодичности rev

--- main/services.py
+++ main/services.py
@@ -1,8 +1,6 @@
 import pytz
 from datetime import datetime, timedelta
-
 from django.core.mail import send_mail
-
 from config import settings
 from main.models import Sending
 
@@ -18,9 +16,6 @@ def send_mailing():
 
     mail_from = settings.EMAIL_HOST_USER
 
-    # print(mail_from)
-    # print(settings.EMAIL_HOST_PASSWORD)
-
     # создание объекта с применением фильтра
     mailings = (Sending.objects.filter(start_time__lte=current_datetime)
                                .filter(status__in=['st',]))
@@ -30,22 +25,24 @@ def send_mailing():
     for mailing in mailings:
         print(f'Активная рассылка: {mailing}')
         print(f'Клиенты по этой рассылке {[client.email for client in mailing.client.all()]}')
-
         print()
 
         time_period = timedelta(minutes=5)  # устанавливаю для тестов в кроне буду проверять каждую минуту
 
+        # Этот блок раскомментировать для работы по расписанию из базы
         # time_period = timedelta(weeks=4)
         # if mailing.period == '1D':
         #     time_period = timedelta(days=1)
         # elif mailing.period == '1W':
         #     time_period = timedelta(weeks=1)
 
+        # принты для проверки
         print(f'Периодичность рассылки {time_period} -> {mailing.period}')
         print(f'Текущее время {current_datetime.strftime(MY_TIME_FORMAT)}')
         print(f'Время последней хорошей рассылки {mailing.last_ok_time.strftime(MY_TIME_FORMAT)}')
         print(mailing.last_ok_time + time_period, '  ', current_datetime)
 
+        # проверка периодичности и рассылка писем
         if mailing.last_ok_time + time_period < current_datetime:
             print('Пора выполнить рассылку')
             mailing.last_ok_time = current_datetime
