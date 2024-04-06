d255e9f6106456649dce40c62c7ef47e08a8dffb
scroco158 <scroco@mail.ru>
2024-04-06 18:30:14 +0300

    отправка по периодичности

--- main/services.py
+++ main/services.py
@@ -1,5 +1,5 @@
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.core.mail import send_mail
 
@@ -15,6 +15,7 @@ def send_mailing():
 
     zone = pytz.timezone('Europe/Moscow')
     current_datetime = datetime.now(zone)
+
     mail_from = settings.EMAIL_HOST_USER
 
     # print(mail_from)
@@ -23,16 +24,35 @@ def send_mailing():
     # создание объекта с применением фильтра
     mailings = (Sending.objects.filter(start_time__lte=current_datetime)
                                .filter(status__in=['st',]))
+
     print('Проверка выбора рассылок и клиентов')
     print()
     for mailing in mailings:
         print(f'Активная рассылка: {mailing}')
         print(f'Клиенты по этой рассылке {[client.email for client in mailing.client.all()]}')
 
-    for mailing in mailings:
-        send_mail(
-            subject=mailing.name,
-            message=mailing.message.body,
-            from_email=mail_from,
-            recipient_list=[client.email for client in mailing.client.all()]
+        print()
+
+        time_period = timedelta(minutes=5)  # устанавливаю для тестов в кроне буду проверять каждую минуту
+
+        # time_period = timedelta(weeks=4)
+        # if mailing.period == '1D':
+        #     time_period = timedelta(days=1)
+        # elif mailing.period == '1W':
+        #     time_period = timedelta(weeks=1)
+
+        print(f'Периодичность рассылки {time_period} -> {mailing.period}')
+        print(f'Текущее время {current_datetime.strftime(MY_TIME_FORMAT)}')
+        print(f'Время последней хорошей рассылки {mailing.last_ok_time.strftime(MY_TIME_FORMAT)}')
+        print(mailing.last_ok_time + time_period, '  ', current_datetime)
+
+        if mailing.last_ok_time + time_period < current_datetime:
+            print('Пора выполнить рассылку')
+            mailing.last_ok_time = current_datetime
+            mailing.save()
+            send_mail(
+                subject=mailing.name,
+                message=mailing.message.body,
+                from_email=mail_from,
+                recipient_list=[client.email for client in mailing.client.all()]
             )
