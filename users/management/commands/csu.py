40364a421502933e642e99eb8de0a36ea494fbc7
scroco158 <scroco@mail.ru>
2024-05-05 18:56:54 +0300

    Аутентификация | создание суперпользователя через кастомную команду

---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        user = User.objects.create(email='admin@admin.ru')
+
+        user.set_password('admin')
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+
