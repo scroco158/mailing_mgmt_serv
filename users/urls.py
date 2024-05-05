6dce46440713289b5bc60af04870b2e2ce2a633c
scroco158 <scroco@mail.ru>
2024-05-05 20:42:27 +0300

    Аутентификация | Login | настроил урлы и добавил шаблон для входа

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from .apps import UsersConfig
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name="users/login.html"), name='login'),
+]
