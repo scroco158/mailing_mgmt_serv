4fd079339fd6e05af23478020393fdbedb055fe6
scroco158 <scroco@mail.ru>
2024-05-05 12:04:25 +0300

    Настройка отправки почты | отредактировал settings.py .env .env.sample

--- config/settings.py
+++ config/settings.py
@@ -140,11 +140,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # настройка почты
-EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_PORT = '465'
-EMAIL_HOST_USER = ''         # os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = ''     # os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = True
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
 
 CRONJOBS = [
     ('*/1 * * * *', 'main.services.send_mailing')
