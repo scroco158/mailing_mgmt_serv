b1a2bc3e0d49a4468faac3d83d280ad7a97c399b
scroco158 <scroco@mail.ru>
2024-05-06 11:30:45 +0300

    Верификация | добавил токен в модель пользователя + миграции

--- users/models.py
+++ users/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 class User(AbstractUser):
     username = None
 
     email = models.EmailField(unique=True, verbose_name="Email Address", max_length=255)
+    token = models.CharField(max_length=100, verbose_name="Token", **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
