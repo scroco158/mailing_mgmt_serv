ad7fdd9cd8b23ebeeaa37e660a035693af43757f
scroco158 <scroco@mail.ru>
2024-05-06 08:22:00 +0300

    Аутентификация | Logout | сделал выход + откатил джаного на 4.2.2

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="users/login.html"), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
