8002d179a8b6d00717e6afdeb0e7ae28247838d2
scroco158 <scroco@mail.ru>
2024-05-06 14:30:23 +0300

    Верификация | верификация по переходу по отправленной ссылке

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .apps import UsersConfig
-from .views import UserCreateView
+from .views import UserCreateView, mail_verification
 
 app_name = UsersConfig.name
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="users/login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
+    path('confirm_email/<str:token>/', mail_verification, name='mail_verification'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,47 @@
-from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import UserCreationForm
 from users.models import User
 
+import secrets
+from django.core.mail import send_mail
+
 
 class UserCreateView(CreateView):
+    """ Создание пользователя """
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        """ Верификация почты пользователя"""
+
+        user = form.save()             # сохранение данные из формы
+        token = secrets.token_hex(16)  # генерация токена
+        user.is_active = False         # устанавливаем пользователя не активным
+        user.token = token             # устанавливаем пользователю токен
+        user.save()                    # сохраняем пользователя
+
+        # формирую ссылку для отправки пользователю
+        host = self.request.get_host()                      # получаю адрес хоста
+        url = f'http://{host}/users/confirm_email/{token}'  # ссылка для отправки
+
+        # отправка письма
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Перейдите по ссылке для подтверждения {url}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email,]
+        )
+        return super().form_valid(form)
+
+
+def mail_verification(request, token):
+    """ Обработка перехода пользователя по отправленной ссылке"""
+    user = get_object_or_404(User, token=token)  # ищем пользователя по токену
+    user.is_active = True                        # если найден - то активируем
+    user.save()                                  # сохраняем
+    return redirect(reverse('users:login'))      # редиректим на вход
